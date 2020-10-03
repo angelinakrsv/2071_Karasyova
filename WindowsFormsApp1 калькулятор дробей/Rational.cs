1bf7756ec1c03f6ed1ba6260f9b3d9bd77a9c4b3
angelinakrsv <71029108+angelinakrsv@users.noreply.github.com>
2020-10-03 11:46:31 +0300

    first commit

---
+++ WindowsFormsApp1 калькулятор дробей/Rational.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace WindowsFormsApp1
+{
+    public class Rational
+    {
+
+        int chislitel;
+        int znamenatel;
+
+        public Rational()
+        {
+
+
+        }
+
+        public Rational Sum(Rational other)
+        {
+            Rational d1 = this;
+            Rational d2 = other;
+        }
+
+
+    }
+}
